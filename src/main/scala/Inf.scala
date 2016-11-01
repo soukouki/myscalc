9659dbd74ff7ce10174af01d73745f4e9d6cc73c
soukouki <soukouki0@yahoo.co.jp>
2016-11-01 16:21:43 +0900

    Infのテストを追加

--- src/main/scala/Inf.scala
+++ src/main/scala/Inf.scala
@@ -1,8 +1,8 @@
 package myscalc.calc
 
 case class Inf() extends Num {
-	override def + (pair: Num): Num = ???
-	override def - (pair: Num): Num = ???
-	override def * (pair: Num): Num = ???
-	override def / (pair: Num): Num = ???
+	override def + (pair: Num) = Inf()
+	override def - (pair: Num) = Inf()
+	override def * (pair: Num) = Inf()
+	override def / (pair: Num) = Inf()
 }
---
+++ src/test/scala/InfSpec.scala
@@ -0,0 +1,8 @@
+import org.scalatest.{FlatSpec, DiagrammedAssertions}
+import myscalc.calc._
+
+class InfSpec extends FlatSpec with DiagrammedAssertions {
+	"+-*/" should "全てInfを返す" in {
+		assert(Inf() + Int(1) === Inf())
+	}
+}
