c177fddf3e24308af58fb04190a46ea811bdbd44
soukouki <soukouki0@yahoo.co.jp>
2016-11-02 00:52:12 +0900

    Parseの単純な式を実装

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -6,10 +6,15 @@ import myscalc.calc._
 object Parse extends RegexParsers {
 	def apply(input: String): Base = parseAll(expr, input) match {
 		case Success(result, _) => result
-		case failure : NoSuccess => scala.sys.error(failure.msg)
+		case failure : NoSuccess => scala.sys.error(failure.msg + "\ninput is \"" + input + "\"")
 	}
 	
-	def expr: Parser[Base] = {
-		"""(\+|-)?\d+""".r ^^ {s => Int(s.toInt)}
-	}
+	def expr: Parser[Base] = chainl1(
+		number,
+		"+" ^^ {op => (l, r) => Add(l, r)} |
+		"-" ^^ {op => (l, r) => Sub(l, r)} |
+		"*" ^^ {op => (l, r) => Mul(l, r)} |
+		"/" ^^ {op => (l, r) => Div(l, r)}
+	)
+	def number: Parser[Base] = """(\+|-)?\d+""".r ^^ {s => Int(s.toInt)}
 }
--- src/test/scala/ParseSpec.scala
+++ src/test/scala/ParseSpec.scala
@@ -7,8 +7,14 @@ class ParseSpec extends FlatSpec with DiagrammedAssertions {
 		assert(Parse("3") === Int(3))
 		assert(Parse("12") === Int(12))
 	}
-	it should "数字の先頭が+と-の場合は、その値で戻る" in {
+	it should "数字の先頭が+と-の場合、その値で戻る" in {
 		assert(Parse("+3") === Int(3))
 		assert(Parse("-3") === Int(-3))
+	}
+	it should "+-*/の単体の場合、それ用のクラスになって戻る" in {
+		assert(Parse("1+1") === Add(Int(1), Int(1)))
+		assert(Parse("1-1") === Sub(Int(1), Int(1)))
+		assert(Parse("1*1") === Mul(Int(1), Int(1)))
+		assert(Parse("1/1") === Div(Int(1), Int(1)))
 	}
 }
