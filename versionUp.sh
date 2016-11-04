f93747887d5299042b6e2c6095657bdd6a76e48c
soukouki <soukouki0@yahoo.co.jp>
2016-11-04 23:27:39 +0900

    githubアップ準備

--- versionUp.sh
+++ versionUp.sh
@@ -7,6 +7,6 @@ sbt assembly
 
 # zipファイルの作成
 mkdir $directory
-cp $(ls target/scala-*/myscalc.jar) $directory
+cp $(ls target/scala-*/myscalc.jar) Readme.md License $directory
 zip -r "myscalc-$newVersion.zip" $directory
 rm -r $directory
