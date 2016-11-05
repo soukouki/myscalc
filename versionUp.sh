673a0e9611565571a179f56eaa2dbedce6a88e26
soukouki <soukouki0@yahoo.co.jp>
2016-11-05 20:59:12 +0900

    迷走中

--- versionUp.sh
+++ versionUp.sh
@@ -8,5 +8,6 @@ sbt assembly
 # zipファイルの作成
 mkdir $directory
 cp $(ls target/scala-*/myscalc.jar) Readme.md License $directory
+echo 'java -jar (join-path (pwd) myscalc.jar) $ARGS' > $directory/myscalc.ps1
 zip -r "myscalc-$newVersion.zip" $directory
 rm -r $directory
