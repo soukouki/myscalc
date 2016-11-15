7a5f6c8ed1ad9691bb87118565e94205af3d2092
soukouki <soukouki0@yahoo.co.jp>
2016-11-16 01:29:42 +0900

    分数の約分ができるように

--- src/main/scala/calc/Num.scala
+++ src/main/scala/calc/Num.scala
@@ -4,8 +4,6 @@ import scala.{Int => ScalaInt}
 import myscalc.calc.operatorbase.MulDivOperator
 
 sealed trait Num extends Base {
-	override def isContinue = false
-	override def result: Num = ???
 	def + (pair: Num): Num
 	def - (pair: Num): Num
 	def * (pair: Num): Num
@@ -16,7 +14,13 @@ object Num {
 }
 
 package num {
-	case class Int(value: ScalaInt) extends Num {
+	/**resultできない[[Num]]*/
+	sealed trait SimpleNum extends Num {
+		override def isContinue = false
+		override def result: Num = ???
+	}
+	
+	case class Int(value: ScalaInt) extends SimpleNum {
 		override def + (pair: Num): Num = pair match {
 			case Int(n) => Int(value + n)
 			case _: Inf => Inf()
@@ -38,21 +42,42 @@ package num {
 			case _: Inf => Inf()
 		}
 		override def string: String = value.toString
+		/**
+			最大公約数
+			{{{a gcd b}}}
+		*/
+		private[num] def gcd(pair: Int): Int = {
+			def gcdi(a: ScalaInt, b: ScalaInt): ScalaInt = {
+				if(b==0) return a
+				gcdi(b, a % b)
+			}
+			Int(gcdi(value, pair.value))
+		}
+		/**[[Rational]]の方で、公約数で割る処理のため*/
+		private[num] def intdiv(pair: Int): Int = Int(value / pair.value)
 	}
 	
-	case class Rational(numerator: Int, denominator: Int) extends Num with MulDivOperator {
-		override def + (pair: Num): Num = ???
-		override def - (pair: Num): Num = ???
-		override def * (pair: Num): Num = ???
-		override def / (pair: Num): Num = ???
-		override def string: String = s"${numerator.string}/${denominator.string}"
-	}
-	
-	case class Inf() extends Num {
+	case class Inf() extends SimpleNum {
 		override def + (pair: Num): Num = Inf()
 		override def - (pair: Num): Num = Inf()
 		override def * (pair: Num): Num = Inf()
 		override def / (pair: Num): Num = Inf()
 		override def string: String = "Inf"
+	}
+	
+	case class Rational(numerator: Int, denominator: Int) extends Num with MulDivOperator {
+		override def isContinue: Boolean = {
+			val gcd = numerator gcd denominator
+			gcd != Int(1)
+		}
+		override def result: Rational = {
+			val gcd = numerator gcd denominator
+			Rational(numerator.intdiv(gcd), denominator.intdiv(gcd))
+		}
+		override def + (pair: Num): Num = ???
+		override def - (pair: Num): Num = ???
+		override def * (pair: Num): Num = ???
+		override def / (pair: Num): Num = ???
+		override def string: String = s"${numerator.string}/${denominator.string}"
 	}
 }
--- src/test/scala/calc/num/RationalSpec.scala
+++ src/test/scala/calc/num/RationalSpec.scala
@@ -3,6 +3,14 @@ import myscalc.calc.num._
 import myscalc.calc.operator._
 
 class RationalSpec extends FlatSpec with DiagrammedAssertions {
+	"result" should "約分できるか" in {
+		assert(Rational(Int(4), Int(6)).result === Rational(Int(2), Int(3)))
+		assert(Rational(Int(100), Int(3)).result === Rational(Int(100), Int(3)))
+	}
+	"isContinue" should "約分ができるときはtrue" in {
+		assert(Rational(Int(2), Int(3)).isContinue === false)
+		assert(Rational(Int(4), Int(6)).isContinue === true)
+	}
 	"+" should "Intを渡されたら"
 	it should "Rationalを渡されたら"
 	"-" should "Intを渡されたら"
