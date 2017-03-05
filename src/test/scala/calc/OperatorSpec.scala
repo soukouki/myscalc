36fc6a177826cb8f24afe587321cc85af501fea9
soukouki <soukouki0@yahoo.co.jp>
2017-03-06 01:31:32 +0900

    numのテストを一つのファイルに纏めた
    ついでにIntSpecを作成しOperatorSpecを綺麗に
    
    FlatCalcTestトライトで、継承するクラスの列挙を一つにまとめた

--- src/test/scala/calc/OperatorSpec.scala
+++ src/test/scala/calc/OperatorSpec.scala
@@ -3,13 +3,13 @@ import myscalc.calc.num._
 import myscalc.calc.operator._
 
 /** [[operator]]共通のテスト */
-class OperatorSpec extends FlatSpec with DiagrammedAssertions {
+class OperatorSpec extends FlatCalcTest {
 	"advance" should "hasFinishedのやつだけになってから処理を始める" in {
 		assert(Add(Rational(Int(5), Int(10)), Int(1)).advance === Add(Rational(Int(1), Int(2)), Int(1)))
 	}
 }
 
-class AddSubOperatorSpec extends FlatSpec with DiagrammedAssertions {
+class AddSubOperatorSpec extends FlatCalcTest {
 	"string" should "右側に+-が来た場合、括弧を付ける" in {
 		assert(Add(Add(Int(1), Int(2)), Add(Int(3), Int(4))).string === "1+2+(3+4)")
 	}
@@ -22,11 +22,8 @@ class AddSubOperatorSpec extends FlatSpec with DiagrammedAssertions {
 	}
 }
 
-class AddSpec extends FlatSpec with DiagrammedAssertions {
-	"advance" should "Intを2つ受け取り、足した数を返す" in {
-		assert(Add(Int(1), Int(2)).advance === Int(3))
-	}
-	it should "式を受け取ると、先に計算する方だけを計算し、返す" in {
+class AddSpec extends FlatCalcTest {
+	"advance" should "式を受け取ると、先に計算する方だけを計算し、返す" in {
 		assert(Add(Add(Int(1), Int(2)), Int(3)).advance === Add(Int(3), Int(3)))
 	}
 	"string" should "式の間に+をつけて返す" in {
@@ -36,12 +33,8 @@ class AddSpec extends FlatSpec with DiagrammedAssertions {
 	}
 }
 
-class SubSpec extends FlatSpec with DiagrammedAssertions {
-	"advance" should "Intを2つ受け取り、引いた値を返す" in {
-		assert(Sub(Int(2), Int(1)).advance === Int(1))
-		assert(Sub(Int(1), Int(2)).advance === Int(-1))
-	}
-	it should "式を受け取ると、先に計算する方だけを計算し、返す" in {
+class SubSpec extends FlatCalcTest {
+	"advance" should "式を受け取ると、先に計算する方だけを計算し、返す" in {
 		assert(Sub(Sub(Int(2), Int(1)), Int(1)).advance === Sub(Int(1), Int(1)))
 	}
 	"string" should "式の間に-をつけて返す" in {
@@ -51,7 +44,7 @@ class SubSpec extends FlatSpec with DiagrammedAssertions {
 	}
 }
 
-class MulDivOperatorSpec extends FlatSpec with DiagrammedAssertions {
+class MulDivOperatorSpec extends FlatCalcTest {
 	"string" should "+-が来た場合、括弧を付ける" in {
 		assert(Mul(Add(Int(1), Int(2)), Sub(Int(3), Int(4))).string === "(1+2)*(3-4)")
 	}
@@ -63,15 +56,8 @@ class MulDivOperatorSpec extends FlatSpec with DiagrammedAssertions {
 	}
 }
 
-class MulSpec extends FlatSpec with DiagrammedAssertions with CalcTestUtility {
-	"advance" should "Mulを2つ受け取り、掛けた数を返す" in {
-		assert(Mul(Int(2), Int(2)).advance === Int(4))
-		assert(Mul(Int(-2), Int(2)).advance === Int(-4))
-		assert(Mul(Int(2), Int(-2)).advance === Int(-4))
-		assert(Mul(Int(-2), Int(-2)).advance === Int(4))
-		assert(Mul(Int(2), Int(0)).advance === Int(0))
-	}
-	it should "式を受け取ると、先に計算する方だけを計算し、返す" in {
+class MulSpec extends FlatCalcTest {
+	"advance" should "式を受け取ると、先に計算する方だけを計算し、返す" in {
 		assert(Mul(Add(Int(2), Int(1)), Int(2)).advance === Mul(Int(3), Int(2)))
 	}
 	it should "片方が1なら、もう片方を返す" in {
@@ -88,16 +74,8 @@ class MulSpec extends FlatSpec with DiagrammedAssertions with CalcTestUtility {
 	}
 }
 
-class DivSpec extends FlatSpec with DiagrammedAssertions with CalcTestUtility {
-	"advance" should "数を1つ受け取り、割って返す" in {
-		assert(Div(Int(4), Int(2)).advance === Int(2))
-		assert(Div(Int(3), Int(1)).advance === Int(3))
-	}
-	it should "割り切れないときは分数にして返す" in {
-		assert(Div(Int(3), Int(2)).advance === Rational(Int(3), Int(2)))
-		assert(Div(Int(3), Int(10)).advance === Rational(Int(3), Int(10)))
-	}
-	it should "0で割った場合、Infを返す" in {
+class DivSpec extends FlatCalcTest {
+	"advance" should "0で割った場合、Infを返す" in {
 		assert(Div(Int(2), Int(0)).advance === Inf())
 		assert(Div(Int(0), Int(0)).advance === Inf())
 	}
